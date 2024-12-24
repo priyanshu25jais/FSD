d2fac788a036af40483ade72d16a562acc79e0ba
priyanshu25jais <jais.priyanshu25@gmail.com>
2024-12-24 14:56:56 +0530

    "Update FSD subproject commit hash from ecbd707 to c52934a."

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import SearchBox from "./searchbox";
+
+function App() {
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <SearchBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ searchbox.jsx
@@ -0,0 +1,6 @@
+export default function SearchBox(){
+    return (
+        <div>
+        <h3>Search for the weather</h3>
+        </div>
+}
